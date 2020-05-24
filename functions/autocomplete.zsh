898b5d61136a7ef6f0553f78628d13234102f1f4
pry <pry@Ghostbook-Air.home>
2020-05-24 19:48:08 +0100

    added autocomplete update

--- functions/autocomplete.zsh
+++ functions/autocomplete.zsh
@@ -1,4 +1,8 @@
-function _axiom(){
+# compdef _axiom-ssh axiom-ssh
+# compdef _axiom-ssh axiom-rm
+# compdef _axiom-ssh axiom-backup
+
+function _axiom-ssh(){
 	local state
 	_arguments "1: :($(doctl compute droplet list | awk '{ print $2 }' | grep -v 'Name' | tr '\n' ' '))"
 }
