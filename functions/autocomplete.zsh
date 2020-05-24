182bf2f2036f7c00bd234820393d53fd56d55c4a
pry <pry@Ghostbook-Air.home>
2020-05-24 20:43:30 +0100

    added tab-completion for restore

--- functions/autocomplete.zsh
+++ functions/autocomplete.zsh
@@ -1,8 +1,14 @@
 # compdef _axiom-ssh axiom-ssh
 # compdef _axiom-ssh axiom-rm
 # compdef _axiom-ssh axiom-backup
+# compdef _axiom-restore axiom-restore
 
 function _axiom-ssh(){
 	local state
 	_arguments "1: :($(doctl compute droplet list | awk '{ print $2 }' | grep -v 'Name' | tr '\n' ' '))"
+}
+
+function _axiom-restore(){
+	local state
+	_arguments "1: :($(ls ~/.axiom/boxes/))"
 }
