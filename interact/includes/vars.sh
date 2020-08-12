122cd7f9e12ab577f120ab62deadf139396ebd36
pry <pry@Ghostbook-Air.home>
2020-08-12 02:08:44 +0100

    added first colors include

---
+++ interact/includes/vars.sh
@@ -0,0 +1,24 @@
+names=("allen""stok" "pry" "chell" "fire" "bango" "cage" "xpn" "luke" "haddix" "omnom" "jobs" "ritchie" "knox" "bleeder" "batman" "blink" "ghost" "metcalf" "hickey" "fightnerd" "pancakes" "kennedy" "jarvis" "unicorn" "medin" "mog" "thl" "cuthbert" "fox" "hunt" "austin" "babbage" "banzai" "bardeen" "bartik" "bassi" "beaver" "bell" "benz" "black""blackwell""booth" "borg" "bose" "boyd" "brown" "buck" "cannon" "carson" "carter" "cerf"  "cohen" "clarke" "colden" "cori" "cray" "curie" "darwin" "davinci" "diffie" "dirac" "edison" "einstein" "elion" "ellis""euclid" "euler" "faraday" "fermat" "fermi" "franklin" "gagarin" "galileo" "gates" "gauss" "goldberg" "goldstine" "goodall" "gould" "greider" "haibt" "hamilton" "haslett" "hawking" "hellman" "heisenberg" "hermann" "herschel" "hertz" "hodgkin" "hoover" "hopper" "hugle" "hypatia" "jackson" "jang" "jennings" "jepsen" "johnson" "joliot" "jones" "kalam" "kare" "keller" "kepler" "kilby" "kirch" "knuth" "lalande" "lamar" "lamport" "leakey" "lederberg" "lehmann" "lewin" "liskov" "lovelace" "margulis" "matsumoto" "maxwell" "mayer" "mccarthy" "mclaren" "mclean" "mcnulty" "mendel" "merkle" "moore" "morse" "murdock" "moser" "napier" "nash" "neumann" "newton" "nightingale" "nobel" "noyce" "panini" "pare" "pascal" "payne" "perlman" "pike" "ptolemy" "raman" "ride" "ritchie" "rhodes" "robinson" "rubin" "saha" "sammet""satoshi" "shamir" "shannon" "shaw" "shirley""snyder" "solomon" "spence" "stonebraker" "sutherland" "swanson" "swartz" "swirles" "tesla""thompson" "torvalds" "tu" "turing" "volhard" "villani" "wescoff" "wilbur" "wiles" "williams""wilson" "wing" "wozniak" "wright" "wu")
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+export Black='\033[0;30m'        # Black
+export Red='\033[0;31m'          # Red
+export Green='\033[0;32m'        # Green
+export Yellow='\033[0;33m'       # Yellow
+export Blue='\033[0;34m'         # Blue
+export Purple='\033[0;35m'       # Purple
+export Cyan='\033[0;36m'         # Cyan
+export White='\033[0;37m'        # White
+
+# Bold
+export BBlack='\033[1;30m'       # Black
+export BRed='\033[1;31m'         # Red
+export BGreen='\033[1;32m'       # Green
+export BYellow='\033[1;33m'      # Yellow
+export BBlue='\033[1;34m'        # Blue
+export BPurple='\033[1;35m'      # Purple
+export BCyan='\033[1;36m'        # Cyan
+export BWhite='\033[1;37m'       # White
