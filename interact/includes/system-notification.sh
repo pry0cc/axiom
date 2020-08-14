58c29942728a69add9f59316120edc747b295b6a
pry0cc <pry0cc@0x00sec.org>
2020-08-14 08:19:30 -0400

    fixed the notifications to work with new method

--- interact/includes/system-notification.sh
+++ interact/includes/system-notification.sh
@@ -6,7 +6,7 @@ BASEOS="$(uname)"
 # method issuing notifications for at least most ubuntu like systems
 # expects a title as first and a notification as second argument
 function notify {
-  notify-send "Axiom" "$1"
+  notify-send "$1" "$2"
 }
 
 # method issuing notifications in place of notify-send on OSX
