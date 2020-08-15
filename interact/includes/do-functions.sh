d358c3a54d50dd8608af08060cb07d42799557ab
pry <pry@Ghostbook-Air.home>
2020-08-15 09:11:15 +0100

    revamped  query_instances - this is  amazingly powerful

--- interact/includes/do-functions.sh
+++ interact/includes/do-functions.sh
@@ -37,3 +37,37 @@ msg_neutral() {
 	echo -e "${Blue}$1${Color_Off}"
 	echo "INFO $(date): $1" >> $LOG
 }
+
+query_instances() {
+	droplets="$(instances)"
+	selected=""
+
+	for var in "$@"; do
+		if [[ "$var" =~ "*" ]]
+		then
+			var=$(echo "$var" | sed 's/*/.*/g')
+			selected="$selected $(echo $droplets | jq -r '.[].name' | grep "$var")"
+		else
+			if [[ $query ]];
+			then
+				query="$query\|$var"
+			else
+				query="$var"
+			fi
+		fi
+	done
+
+	if [[ "$query" ]]
+	then
+		selected="$selected $(echo $droplets | jq -r '.[].name' | grep -w "$query")"
+	else
+		if [[ ! "$selected" ]]
+		then
+			echo -e "${Red}No instance supplied, use * if you want to delete all instances...${Color_Off}"
+			exit
+		fi
+	fi
+
+	selected=$(echo "$selected" | tr ' ' '\n' | sort -u)
+	echo $selected
+}
