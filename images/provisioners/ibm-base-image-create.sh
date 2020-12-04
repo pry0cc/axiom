858cd05c1e4863fd00f00ea99a794c106f59c76d
0xtavian <21030907+0xtavian@users.noreply.github.com>
2020-12-04 15:54:03 -0600

    Update ibm-base-image-create.sh

--- images/provisioners/ibm-base-image-create.sh
+++ images/provisioners/ibm-base-image-create.sh
@@ -1,17 +1,17 @@
 if [ -f ~/.ssh/id_rsa.pub ] || [ -f ~/.ssh/id_ed25519.pub ]; then
-	echo -e "Detected SSH public key, would you like me to install this for your axiom setup? y/n"
-	read ans
-	if [ $ans == "n" ]; then
-		echo -e "Would you like to generate a fresh pair? y/n"
+echo -e "${Blue}Detected SSH public key, would you like me to install this for your axiom setup? y\/n"
 		read ans
-		if [ $ans == "y" ]; then
-			ssh-keygen -b 2048 -t rsa -q -N ""
-		else
-			echo -e "Need to provide SSH public key. Exiting..."
-			exit 0
-		fi
+		if [ $ans == "n" ]; then
+			echo -e "Would you like to generate a fresh pair? y/n"
+			read ans
+			if [ $ans == "y" ]; then
+				ssh-keygen -b 2048 -t rsa -q -N ""
+			else
+				echo -e "Need to provide SSH public key. Exiting..."
+				exit 0
+			fi
 
-	fi
+		fi
 else
 	echo -e "No SSH public key detected, would you like to generate a fresh pair? y/n"
 	read ans
@@ -22,13 +22,13 @@ else
 			exit 0
 	fi
 fi
-
+axiomhostname=axiom-controller$RANDOM
 key=$(cat ~/.ssh/id_rsa.pub)
 inuse=$(ibmcloud sl security sshkey-list -output json | jq --arg v "$key" '.[] | select(.key | contains($v))' | jq .key | tr -d '"')
 
-if [ "$key" = "$inuse" ] 
+if [ "$key" = "$inuse" ] ;
 then
-    sshkeyid=$(ibmcloud sl security sshkey-list -output json | jq --arg v "$key" '.[] | select(.key | contains($v))' | jq .id); # SSH key already exists in IBM Cloud, just get the ID
+    sshkeyid=$(ibmcloud sl security sshkey-list -output json | jq --arg v "$key" '.[] | select(.key | contains($v))' | jq ".id") # SSH key already exists in IBM Cloud, just get the ID
 else
     sshkeyid=$(ibmcloud sl security sshkey-add axiom-ssh-key -f ~/.ssh/id_rsa.pub -output json | jq '.id' 3>&1); # Upload your SSH key to IBM Cloud & Get ID
 fi
@@ -37,7 +37,7 @@ echo "/n"
 echo "Uploading SSH key"
 echo "SSH Key ID $sshkeyid"; # ssh key id
 echo "Creating Ubuntu 18.04 Base VSI";
-id=$(echo "Y"| ibmcloud sl vs create -H axiom-base-image -D axiom.local -c 2 -m 4096 -d dal13 -o UBUNTU_18_64 --disk 100 -k $sshkeyid | grep -s "^ID" | tr -s " " | cut -d " " -f 2 ); # create axiom base image with sshkey
+id=$(echo "Y"| ibmcloud sl vs create -H $axiomhostname -D axiom.local -c 2 -m 4096 -d dal13 -o UBUNTU_18_64 --disk 100 -k $sshkeyid | grep -s "^ID" | tr -s " " | cut -d " " -f 2 ); # create axiom base image with sshkey
 echo "Base IBM Cloud Server ID is $id";
 echo "sleeping one minute";
 sleep 60;
