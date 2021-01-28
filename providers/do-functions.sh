bc1d13928175b3d455c4e898bca6f5b925d5afa7
0xtavian <21030907+0xtavian@users.noreply.github.com>
2021-01-27 22:42:41 -0600

    Update do-functions.sh

--- providers/do-functions.sh
+++ providers/do-functions.sh
@@ -242,6 +242,8 @@ generate_sshconfig() {
 		ip=$(echo "$droplets" | jq -r ".[] | select(.name==\"$name\") | .networks.v4[] | select(.type==\"public\") | .ip_address")
 		echo -e "Host $name\n\tHostName $ip\n\tUser op\n\tPort 2266\n" >> $AXIOM_PATH/.sshconfig.new
     echo -e "ServerAliveInterval 60" >> $AXIOM_PATH/.sshconfig.new
+    echo -e "Host *\n\tControlMaster auto\n\tControlPath  ~/.ssh/sockets/%r@%h-%p\n\tControlPersist 600" >> $AXIOM_PATH/.sshconfig.new
+
 	done
 	mv $AXIOM_PATH/.sshconfig.new $AXIOM_PATH/.sshconfig
 	
