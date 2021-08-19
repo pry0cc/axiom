96492e2a8e8475da20f369d708072b8df68c35c4
0xtavian <21030907+0xtavian@users.noreply.github.com>
2021-08-18 19:58:43 -0500

    Update do.sh

--- interact/account-helpers/do.sh
+++ interact/account-helpers/do.sh
@@ -41,8 +41,6 @@ brew install doctl
 elif [[ $BASEOS == "Linux" ]]; then
 OS=$(lsb_release -i | awk '{ print $3 }')
    if ! command -v lsb_release &> /dev/null; then
-            echo "WARNING: This install might not work"
-            echo "lsb_release could not be found, unable to determine your distribution"
             OS="unknown-Linux"
             BASEOS="Linux"
    fi
