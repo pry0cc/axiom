b36a0d7b32d07d7264277315673f8418c1f857a2
0xtavian <21030907+0xtavian@users.noreply.github.com>
2021-08-18 19:58:24 -0500

    Update azure.sh

--- interact/account-helpers/azure.sh
+++ interact/account-helpers/azure.sh
@@ -47,8 +47,6 @@ brew update && brew install azure-cli
 elif [ $BASEOS == "Linux" ]; then
 OS=$(lsb_release -i | awk '{ print $3 }')
    if ! command -v lsb_release &> /dev/null; then
-            echo "ERROR: This install might not work"
-            echo "lsb_release could not be found, unable to determine your distribution"
             OS="unknown-Linux"
             BASEOS="Linux"
    fi
